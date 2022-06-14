76500440f683926a239464eb32d4fcdb3dbaa402
Natan Barboza <natanbarboza084@gmail.com>
2022-06-14 16:57:24 -0300

    first commit

---
+++ funcoes.js
@@ -0,0 +1,7 @@
+function soma(a, b) {
+    return a + b; //15
+}
+
+var main = soma(10, 5);
+
+console.log(main);
---
+++ manipulando_arquivo.js
@@ -0,0 +1,12 @@
+// Boas Práticas 
+
+/*  Declaração de constantes em letras maiúsculas;
+    Comentários de linha com dupla barra;
+    Comentários com mais de uma linha com barra e asterisco;
+*/
+
+var preco = 2;
+var deconto = 0.2;
+const PRECO = 2;
+
+var total = PRECO - desconto;
